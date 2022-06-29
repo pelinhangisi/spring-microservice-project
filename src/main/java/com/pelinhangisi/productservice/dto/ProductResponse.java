428528841ecef38909485794d502fedb9c07150d
pelinhangisi <pelinhangisi@gmail.com>
2022-06-29 14:01:18 +0300

    ProductResponse dto oluşturuldu

--- src/main/java/com/pelinhangisi/productservice/dto/ProductResponse.java
+++ src/main/java/com/pelinhangisi/productservice/dto/ProductResponse.java
@@ -1,4 +1,20 @@
 package com.pelinhangisi.productservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 }
