4ee71dae1804d02fd6c9f0b4414f7f02d3b089f3
pelinhangisi <pelinhangisi@gmail.com>
2022-06-29 15:01:58 +0300

    ProductController GetMapping endpointi güncellendi

--- src/main/java/com/pelinhangisi/productservice/controller/ProductController.java
+++ src/main/java/com/pelinhangisi/productservice/controller/ProductController.java
@@ -17,17 +17,18 @@ public class ProductController {
 
     private final ProductService productService;
 
-    // Product creat etmek için oluşturulan kısım
+    // Product create etmek için oluşturulan endpoint
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
         productService.createProduct(productRequest);
     }
 
+    // bütün Productları listemek adına oluşturulan endpoint.
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts(){
-        productService.getAllProducts();
+        return productService.getAllProducts();
     }
 
 }
